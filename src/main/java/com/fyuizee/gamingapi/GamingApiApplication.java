f6bcc28d15e5397c000e733ff3513bd954e364c0
ilya.sedelnikov <ilya@starexchangealliance.com>
2025-05-06 21:59:45 +0200

    first commit

---
+++ src/main/java/com/fyuizee/gamingapi/GamingApiApplication.java
@@ -0,0 +1,13 @@
+package com.fyuizee.gamingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fyuizee/gamingapi/GamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyuizee.gamingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
